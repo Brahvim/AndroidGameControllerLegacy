7631fef3444afb8b6cb69338001d96d5875025ef
Brahvim <69293652+Brahvim@users.noreply.github.com>
2022-11-05 23:39:13 +0530

    Ah yes, ONE change 😂

--- src/com/brahvim/androidgamecontroller/serial/config/ConfigurationPacket.java
+++ src/com/brahvim/androidgamecontroller/serial/config/ConfigurationPacket.java
@@ -1,5 +1,10 @@
 package com.brahvim.androidgamecontroller.serial.config;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 
@@ -39,4 +44,74 @@ public class ConfigurationPacket implements Serializable {
         return (T)p_object;
     }
 
+    // We return a new object instead so you can *revert if needed or something I dunno:*
+    public static ConfigurationPacket parse(File p_file) {
+        //int lineNumber = 0;
+
+        ConfigurationPacket ret = new ConfigurationPacket();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(p_file))) {
+            String section = "", content = "";
+            int eqPos, lineLen;
+
+            for (String line; (line = reader.readLine()) != null; /*lineNumber++*/) {
+                switch (line.charAt(0)) {
+                    case '#':
+                    case ';':
+                        continue;
+                    case '[':
+                        section = line.substring(1, line.indexOf(']'));
+                        break;
+                    default:
+                        break;
+                }
+
+                lineLen = line.length();
+
+                eqPos = line.indexOf('=');
+                content = line.substring(eqPos + 1, lineLen);
+
+                switch (section) {
+                    case "Button" -> {
+                    }
+
+                    case "DpadButton" -> {
+
+                    }
+
+                    case "Thumbstick" -> {
+                    }
+
+                    case "Touchpad" -> {
+                    }
+
+                    case "Settings" -> {
+                    }
+
+                    // region Quick-settings buttons.
+                    case "QuickSettings1" -> {
+                    }
+
+                    case "QuickSettings2" -> {
+                    }
+
+                    case "QuickSettings3" -> {
+                    }
+                    // endregion
+
+                    default -> {
+                    }
+                }
+            }
+        } catch (
+          FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (
+          IOException p_e) {
+            p_e.printStackTrace();
+            return null;
+        }
+
+        return ret;
+    }
 }
